51575f457cb5fdc09c7ec556e5cfda2c779e2304
Drew Pereli <drewpereli@gmail.com>
2021-02-01 22:23:00 -0800

    Added Board model

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const localtunnel = require('localtunnel');
+const { Board } = require('./models');
 
 const server = express();
 
 server.use(express.json()); // for parsing application/json
 
 server.post('/register_device', async (req, res) => {
-    console.log(req.body);
-    res.send('foo');
+  let { boardId: id, ipAddress } = req.body;
+  await Board.create({ id, ipAddress });
+  res.sendStatus(200);
 });
 
+(async () => {
+  await server.listen(process.env.SERVER_PORT);
+  console.log('Server listening on port ' + process.env.SERVER_PORT);
 
-server.listen(process.env.SERVER_PORT);
-
-localtunnel({ port: process.env.SERVER_PORT, subdomain: process.env.SERVER_SUBDOMAIN });
+  let tunnel = await localtunnel({
+    port: process.env.SERVER_PORT,
+    subdomain: process.env.SERVER_SUBDOMAIN,
+  });
+  console.log('Localtunnel started at ' + tunnel.url);
+})();
---
+++ migrations/20210202055513-create-board.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Boards', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      ipAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Boards');
+  },
+};
