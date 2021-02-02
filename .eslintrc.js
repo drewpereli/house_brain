aa878d9490896a23eba81acbdbd231087fa923df
Drew Pereli <drewpereli@gmail.com>
2021-02-01 22:16:11 -0800

    Install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,25 @@
 module.exports = {
-    'env': {
-        'commonjs': true,
-        'es2021': true,
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 12
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+
+  plugins: ['prettier'],
+
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        useTabs: false,
+      },
+    ],
+  },
 };
